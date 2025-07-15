00926a20ea9176c5f8e9ceea7d6dd5e04813579a
sourabh <shuklasourabh50@gmail.com>
2025-07-16 00:48:47 +0530

    mail sender issue resolved

--- Billing-service/src/test/java/Billing/service/Billing/service/BillingServiceApplicationTests.java
+++ Billing-service/src/test/java/Billing/service/Billing/service/BillingServiceApplicationTests.java
@@ -2,11 +2,16 @@ package Billing.service.Billing.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class BillingServiceApplicationTests {
+
+	@MockBean
+	private JavaMailSender javaMailSender;
 
 	@Test
 	void contextLoads() {
